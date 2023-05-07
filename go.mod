6c63eac0cfa2881d878b0bc3aed1b24af1b287de
mertcaliskanlnx <mertcaliskanlinux@gmail.com>
2023-05-07 05:37:55 +0300

    v

--- go.mod
+++ go.mod
@@ -1,5 +1,8 @@
-module server/main.go
+module github.com/mertcaliskanlnx/golang-grpc-v1
 
 go 1.16
 
-require github.com/golang/protobuf v1.5.3
+require (
+	github.com/golang/protobuf v1.5.3
+	google.golang.org/grpc v1.55.0
+)
--- server/main.go
+++ server/main.go
@@ -1,8 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"time"
+
+	pb "github.com/mertcaliskanlnx/golang-grpc-v1/server/pb"
+	"google.golang.org/grpc"
+)
 
 func main() {
 
+	lis, err := net.Listen("tcp", ":8080")
+
+	if err != nil {
+		log.Fatalf("Failed to listen on port 8080: %v", err)
+	}
+	server := grpc.NewServer()
+
+	pb.RegisterUserServiceServer(server, &srv{})
+
 	fmt.Println("GRPC - Server V1.0.0 is   running...")
+	err = server.Serve(lis)
+	log.Fatal(err)
+}
+
+type srv struct{}
+
+func (s *srv) Now(ctx context.Context, req *pb.NowRequest) (*pb.NowResponse, error) {
+	return &pb.TimeResponse{Time: &pb.Time{
+		Value: time.Now().String(),
+	}}, nil
+}
+
+func (s *srv) Stream(req *pb.StreamRequest, stream pb.UserService_StreamServer) error {
+	deadline := time.Now().Add(time.Duration(req.Length()) * time.Second)
+
+	for !time.Now().After(deadline) {
+		stream.Send(&pb.TimeResponse{Time: &pb.Time{
+			Value: time.Now().String(),
+		}})
+		time.Sleep(1 * time.Second)
+	}
+	return nil
 }
