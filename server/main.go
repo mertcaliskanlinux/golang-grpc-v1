b11a880d2261891e1077c9fd135c72691cbe68bb
mertcaliskanlnx <mertcaliskanlinux@gmail.com>
2023-05-07 12:51:13 +0300

    update service

--- server/main.go
+++ server/main.go
@@ -2,46 +2,47 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"net"
 	"time"
 
-	pb "github.com/mertcaliskanlnx/golang-grpc-v1/server/pb"
+	"github.com/mertcaliskanlnx/golang-grpc-v1/server/pb"
+
 	"google.golang.org/grpc"
 )
 
 func main() {
-
-	lis, err := net.Listen("tcp", ":8080")
-
+	lis, err := net.Listen("tcp", "localhost:8080")
 	if err != nil {
-		log.Fatalf("Failed to listen on port 8080: %v", err)
+		log.Fatalf("failed to listen: %v", err)
 	}
 
 	server := grpc.NewServer()
 
-	fmt.Println("GRPC - Server V1.0.0 is   running...")
+	// Proto'da tanimladigimiz TimeService servisini implement etmek istedigimizi bu sekilde belirtiyoruz.
+	pb.RegisterTimeServiceServer(server, &srv{})
+	log.Printf("starting gRPC server")
 	err = server.Serve(lis)
 	log.Fatal(err)
 }
 
 type srv struct{}
 
-func (s *srv) Now(ctx context.Context, req *pb.TimeStreamRequest) (*pb.TimeUpdate, error) {
+func (s *srv) Now(ctx context.Context, req *pb.NowRequest) (*pb.TimeUpdate, error) {
 	return &pb.TimeUpdate{Time: &pb.Time{
 		Value: time.Now().String(),
 	}}, nil
 }
 
-func (s *srv) Stream(req *pb.TimeStreamRequest, stream pb.TimeStreamRequest) error {
-
+func (s *srv) Stream(req *pb.TimeStreamRequest, stream pb.TimeService_StreamServer) error {
+	// bir deadline belirleyip sonuna kadar client'a veri gonderiyoruz (server stream)
 	deadline := time.Now().Add(time.Duration(req.Length) * time.Second)
-
 	for !time.Now().After(deadline) {
 		time.Sleep(time.Millisecond * 300)
 
-		fmt.Println("ff")
+		stream.Send(&pb.TimeUpdate{Time: &pb.Time{
+			Value: time.Now().String(),
+		}})
 	}
 	return nil
 }
--- server/pb/timeservice.pb.go
+++ server/pb/timeservice.pb.go
@@ -102,36 +102,36 @@ func (m *TimeUpdate) GetTime() *Time {
 	return nil
 }
 
-type Nowrequest struct {
+type NowRequest struct {
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
 }
 
-func (m *Nowrequest) Reset()         { *m = Nowrequest{} }
-func (m *Nowrequest) String() string { return proto.CompactTextString(m) }
-func (*Nowrequest) ProtoMessage()    {}
-func (*Nowrequest) Descriptor() ([]byte, []int) {
+func (m *NowRequest) Reset()         { *m = NowRequest{} }
+func (m *NowRequest) String() string { return proto.CompactTextString(m) }
+func (*NowRequest) ProtoMessage()    {}
+func (*NowRequest) Descriptor() ([]byte, []int) {
 	return fileDescriptor_c24d50486e4ed4c3, []int{2}
 }
 
-func (m *Nowrequest) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_Nowrequest.Unmarshal(m, b)
+func (m *NowRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_NowRequest.Unmarshal(m, b)
 }
-func (m *Nowrequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_Nowrequest.Marshal(b, m, deterministic)
+func (m *NowRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_NowRequest.Marshal(b, m, deterministic)
 }
-func (m *Nowrequest) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_Nowrequest.Merge(m, src)
+func (m *NowRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_NowRequest.Merge(m, src)
 }
-func (m *Nowrequest) XXX_Size() int {
-	return xxx_messageInfo_Nowrequest.Size(m)
+func (m *NowRequest) XXX_Size() int {
+	return xxx_messageInfo_NowRequest.Size(m)
 }
-func (m *Nowrequest) XXX_DiscardUnknown() {
-	xxx_messageInfo_Nowrequest.DiscardUnknown(m)
+func (m *NowRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_NowRequest.DiscardUnknown(m)
 }
 
-var xxx_messageInfo_Nowrequest proto.InternalMessageInfo
+var xxx_messageInfo_NowRequest proto.InternalMessageInfo
 
 type TimeStreamRequest struct {
 	Length               int32    `protobuf:"varint,1,opt,name=length,proto3" json:"length,omitempty"`
@@ -175,7 +175,7 @@ func (m *TimeStreamRequest) GetLength() int32 {
 func init() {
 	proto.RegisterType((*Time)(nil), "Time")
 	proto.RegisterType((*TimeUpdate)(nil), "TimeUpdate")
-	proto.RegisterType((*Nowrequest)(nil), "Nowrequest")
+	proto.RegisterType((*NowRequest)(nil), "NowRequest")
 	proto.RegisterType((*TimeStreamRequest)(nil), "TimeStreamRequest")
 }
 
@@ -184,20 +184,20 @@ func init() {
 }
 
 var fileDescriptor_c24d50486e4ed4c3 = []byte{
-	// 194 bytes of a gzipped FileDescriptorProto
+	// 193 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0x2c, 0xc9, 0xcc, 0x4d,
 	0x2d, 0x4e, 0x2d, 0x2a, 0xcb, 0x4c, 0x4e, 0xd5, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x57, 0x92, 0xe1,
 	0x62, 0x09, 0xc9, 0xcc, 0x4d, 0x15, 0x12, 0xe1, 0x62, 0x2d, 0x4b, 0xcc, 0x29, 0x4d, 0x95, 0x60,
 	0x54, 0x60, 0xd4, 0xe0, 0x0c, 0x82, 0x70, 0x94, 0xd4, 0xb9, 0xb8, 0x40, 0xb2, 0xa1, 0x05, 0x29,
 	0x89, 0x25, 0xa9, 0x42, 0x92, 0x5c, 0x2c, 0x20, 0x03, 0xc0, 0x4a, 0xb8, 0x8d, 0x58, 0xf5, 0x40,
-	0x52, 0x41, 0x60, 0x21, 0x25, 0x1e, 0x2e, 0x2e, 0xbf, 0xfc, 0xf2, 0xa2, 0xd4, 0xc2, 0xd2, 0xd4,
-	0xe2, 0x12, 0x25, 0x6d, 0x2e, 0x41, 0x90, 0x5c, 0x70, 0x49, 0x51, 0x6a, 0x62, 0x6e, 0x10, 0x44,
-	0x50, 0x48, 0x8c, 0x8b, 0x2d, 0x27, 0x35, 0x2f, 0xbd, 0x24, 0x03, 0xac, 0x9f, 0x35, 0x08, 0xca,
-	0x33, 0x8a, 0xe6, 0xe2, 0x06, 0x2b, 0x86, 0x38, 0x4b, 0x48, 0x9e, 0x8b, 0xd9, 0x2f, 0xbf, 0x5c,
-	0x88, 0x5b, 0x0f, 0x61, 0x9e, 0x14, 0xb7, 0x1e, 0x92, 0x2b, 0xb4, 0xb9, 0xd8, 0x20, 0x06, 0x0b,
-	0x09, 0xe9, 0x61, 0xd8, 0x82, 0xa2, 0xd4, 0x80, 0xd1, 0x89, 0x25, 0x8a, 0xa9, 0x20, 0x29, 0x89,
-	0x0d, 0xec, 0x57, 0x63, 0x40, 0x00, 0x00, 0x00, 0xff, 0xff, 0x8e, 0x7a, 0xdd, 0x51, 0x00, 0x01,
-	0x00, 0x00,
+	0x52, 0x41, 0x60, 0x21, 0x25, 0x1e, 0x2e, 0x2e, 0xbf, 0xfc, 0xf2, 0xa0, 0xd4, 0xc2, 0xd2, 0xd4,
+	0xe2, 0x12, 0x25, 0x6d, 0x2e, 0x41, 0x90, 0x5c, 0x70, 0x49, 0x51, 0x6a, 0x62, 0x2e, 0x54, 0x50,
+	0x48, 0x8c, 0x8b, 0x2d, 0x27, 0x35, 0x2f, 0xbd, 0x24, 0x03, 0xac, 0x9f, 0x35, 0x08, 0xca, 0x33,
+	0x8a, 0xe6, 0xe2, 0x06, 0x2b, 0x86, 0x38, 0x4b, 0x48, 0x9e, 0x8b, 0xd9, 0x2f, 0xbf, 0x5c, 0x88,
+	0x5b, 0x0f, 0x61, 0x9e, 0x14, 0xb7, 0x1e, 0x92, 0x2b, 0xb4, 0xb9, 0xd8, 0x20, 0x06, 0x0b, 0x09,
+	0xe9, 0x61, 0xd8, 0x82, 0xa2, 0xd4, 0x80, 0xd1, 0x89, 0x25, 0x8a, 0xa9, 0x20, 0x29, 0x89, 0x0d,
+	0xec, 0x57, 0x63, 0x40, 0x00, 0x00, 0x00, 0xff, 0xff, 0xe7, 0xf5, 0x14, 0xc8, 0x00, 0x01, 0x00,
+	0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -212,7 +212,11 @@ const _ = grpc.SupportPackageIsVersion6
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type TimeServiceClient interface {
-	Now(ctx context.Context, in *Nowrequest, opts ...grpc.CallOption) (*TimeUpdate, error)
+	Now(ctx context.Context, in *NowRequest, opts ...grpc.CallOption) (*TimeUpdate, error)
+	// asagidaki bir server stream ornegidir. http2 sayesinde tek bir tcp baglantisi uzerinden
+	// websocketlerde oldugu gibi defaatle veri gonderip alma islemi yapabiliyoruz.
+	// asagidaki stream TimeUpdate ifadesi, server'in TimeUpdate tipinde veri stream edecegini
+	// yani defaatle bu tipte bir veriyi donebilecegini belirtiyor.
 	Stream(ctx context.Context, in *TimeStreamRequest, opts ...grpc.CallOption) (TimeService_StreamClient, error)
 }
 
@@ -224,7 +228,7 @@ func NewTimeServiceClient(cc grpc.ClientConnInterface) TimeServiceClient {
 	return &timeServiceClient{cc}
 }
 
-func (c *timeServiceClient) Now(ctx context.Context, in *Nowrequest, opts ...grpc.CallOption) (*TimeUpdate, error) {
+func (c *timeServiceClient) Now(ctx context.Context, in *NowRequest, opts ...grpc.CallOption) (*TimeUpdate, error) {
 	out := new(TimeUpdate)
 	err := c.cc.Invoke(ctx, "/TimeService/Now", in, out, opts...)
 	if err != nil {
@@ -267,7 +271,11 @@ func (x *timeServiceStreamClient) Recv() (*TimeUpdate, error) {
 
 // TimeServiceServer is the server API for TimeService service.
 type TimeServiceServer interface {
-	Now(context.Context, *Nowrequest) (*TimeUpdate, error)
+	Now(context.Context, *NowRequest) (*TimeUpdate, error)
+	// asagidaki bir server stream ornegidir. http2 sayesinde tek bir tcp baglantisi uzerinden
+	// websocketlerde oldugu gibi defaatle veri gonderip alma islemi yapabiliyoruz.
+	// asagidaki stream TimeUpdate ifadesi, server'in TimeUpdate tipinde veri stream edecegini
+	// yani defaatle bu tipte bir veriyi donebilecegini belirtiyor.
 	Stream(*TimeStreamRequest, TimeService_StreamServer) error
 }
 
@@ -275,7 +283,7 @@ type TimeServiceServer interface {
 type UnimplementedTimeServiceServer struct {
 }
 
-func (*UnimplementedTimeServiceServer) Now(ctx context.Context, req *Nowrequest) (*TimeUpdate, error) {
+func (*UnimplementedTimeServiceServer) Now(ctx context.Context, req *NowRequest) (*TimeUpdate, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Now not implemented")
 }
 func (*UnimplementedTimeServiceServer) Stream(req *TimeStreamRequest, srv TimeService_StreamServer) error {
@@ -287,7 +295,7 @@ func RegisterTimeServiceServer(s *grpc.Server, srv TimeServiceServer) {
 }
 
 func _TimeService_Now_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(Nowrequest)
+	in := new(NowRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
@@ -299,7 +307,7 @@ func _TimeService_Now_Handler(srv interface{}, ctx context.Context, dec func(int
 		FullMethod: "/TimeService/Now",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(TimeServiceServer).Now(ctx, req.(*Nowrequest))
+		return srv.(TimeServiceServer).Now(ctx, req.(*NowRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
